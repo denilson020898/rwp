a4f40571014ca7b2f2b554467cc0846304c2a42d
Denilson <denilson020898@gmail.com>
2021-06-01 16:48:50 +0700

    user serde_json to read file

--- c02/todo_app/src/main.rs
+++ c02/todo_app/src/main.rs
@@ -1,16 +1,19 @@
+mod state;
 mod to_do;
 
-use to_do::ItemTypes;
-use to_do::to_do_factory;
-
-use crate::to_do::structs::traits::create::Create;
+use serde_json::value::Value;
+use serde_json::{json, Map};
+use state::{read_file, write_to_file};
+use std::env;
 
 fn main() {
-    let to_do_item = to_do_factory("pending", "masturbate");
+    let args: Vec<String> = env::args().collect();
+    let status: &String = &args[1];
+    let title: &String = &args[2];
 
-    match to_do_item.unwrap() {
-        ItemTypes::Pending(item) => item.create(&item.super_struct.title),
-        ItemTypes::Done(item) => println!("it is a pending item with title: {}", item.super_struct.title)
-    }
+    let mut state: Map<String, Value> = read_file(&String::from("./state.json"));
+    println!("{:?}", state);
+
+    // state.insert(title.to_string(), json!(status));
+    // write_to_file("./state.json", &mut state);
 }
-
