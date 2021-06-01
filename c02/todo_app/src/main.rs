5bd235b80da1afda59683227f5f3fbd56e2ecd93
Denilson <denilson020898@gmail.com>
2021-06-01 14:32:33 +0700

    add traits functionality

--- c02/todo_app/src/main.rs
+++ c02/todo_app/src/main.rs
@@ -3,12 +3,14 @@ mod to_do;
 use to_do::ItemTypes;
 use to_do::to_do_factory;
 
+use crate::to_do::structs::traits::create::Create;
+
 fn main() {
-    let to_do_item = to_do_factory("done", "masturbate");
+    let to_do_item = to_do_factory("pending", "masturbate");
 
     match to_do_item.unwrap() {
-        ItemTypes::Done(item) => println!("it is a done item with title: {}", item.super_struct.title),
-        ItemTypes::Pending(item) => println!("it is a pending item with title: {}", item.super_struct.title)
+        ItemTypes::Pending(item) => item.create(&item.super_struct.title),
+        ItemTypes::Done(item) => println!("it is a pending item with title: {}", item.super_struct.title)
     }
 }
 
