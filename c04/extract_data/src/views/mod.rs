3803c9ac2e51b055875dd256983a880e757651dd
Denilson <denilson020898@gmail.com>
2021-06-12 17:04:14 +0700

    edit data using PUT HTTP method and add token

---
+++ c04/extract_data/src/views/mod.rs
@@ -0,0 +1,11 @@
+mod auth;
+mod path;
+mod to_do;
+pub mod token;
+
+use actix_web::web;
+
+pub fn views_factory(app: &mut web::ServiceConfig) {
+    auth::auth_factory(app);
+    to_do::item_factory(app);
+}
