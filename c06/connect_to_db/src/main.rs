ac1d0fe37be6dcd7bfecfdad506a0f3795658c80
Denilson <denilson020898@gmail.com>
2021-06-22 00:18:31 +0700

    chapter 06 connect to db

---
+++ c06/connect_to_db/src/main.rs
@@ -0,0 +1,38 @@
+#[macro_use] extern crate diesel;
+extern crate dotenv;
+
+use actix_service::Service;
+use actix_web::{App, HttpServer};
+mod json_ser;
+mod processes;
+mod state;
+mod to_do;
+mod views;
+mod schema;
+mod database;
+mod models;
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let app = App::new()
+            .wrap_fn(|req, srv| {
+                if *&req.path().contains("/item/") {
+                    match views::token::process_token(&req) {
+                        Ok(_token) => println!("the token is passable"),
+                        Err(msg) => println!("token error: {}", msg),
+                    }
+                }
+                let fut = srv.call(req);
+                async {
+                    let result = fut.await?;
+                    Ok(result)
+                }
+            })
+            .configure(views::views_factory);
+        return app;
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
