d94c86a00e4834ea64731105334ee94ec5718009
Denilson <denilson020898@gmail.com>
2021-06-27 23:45:24 +0700

    add another link to be active /login

--- c08/src/views/app/mod.rs
+++ c08/src/views/app/mod.rs
@@ -6,16 +6,11 @@ mod logout;
 use actix_web::web;
 use super::path::Path;
 
-// pub fn app_factory(app: &mut web::ServiceConfig) {
-//     let base_path: Path = Path{ prefix: String::from("/") };
-//     app.route(&base_path.define(String::from("")), web::get().to(items::items));
-//     app.route(&base_path.define(String::from("login")), web::get().to(login::login));
-//     app.route(&base_path.define(String::from("logout")), web::get().to(logout::logout));
-// }
-
 pub fn app_factory(app: &mut web::ServiceConfig) {
-    let base_path: Path = Path{prefix: String::from("/")};
+    let base_path: Path = Path{ prefix: String::from("/") };
     app.route(&base_path.define(String::from("")), web::get().to(items::items));
+    app.route(&base_path.define(String::from("bro")), web::get().to(login::login));
     app.route(&base_path.define(String::from("login")), web::get().to(login::login));
     app.route(&base_path.define(String::from("logout")), web::get().to(logout::logout));
 }
+
