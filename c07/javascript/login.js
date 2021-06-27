e4bb994e5c533a42f3c4ae2c32e5209c02284c52
Denilson <denilson020898@gmail.com>
2021-06-27 14:02:20 +0700

    start login js and login html

---
+++ c07/javascript/login.js
@@ -0,0 +1,4 @@
+const loginButton = document.getElementById("loginButton");
+const username = document.getElementById("defaultLoginFormUsername");
+const password = document.getElementById("defaultLoginFormPassword");
+const message = document.getElementById("loginMessage");
